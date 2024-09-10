a245b75584cd327ed270671902595d696826c780
Eric <lionhuang914903@gmail.com>
2024-09-10 15:11:12 +0800

    Added: problem 00299

---
+++ CPE_problems/00299.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define ouo ios_base::sync_with_stdio(false), cin.tie(0)
+#define ll long long
+#define db double
+#define pii pair<int, int>
+#define pdd pair<double, double>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) cin >> arr[i];
+
+        int cnt = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr[j], arr[j + 1]);
+                    cnt++;
+                }
+            }
+        }
+
+        printf("Optimal train swapping takes %d swaps.\n", cnt);
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ## Problems
 
 [00100 - The 3n + 1 problem](https://onlinejudge.org/external/1/100.pdf)
-[00272 - TeX Quotes]()
+[00272 - TeX Quotes](https://onlinejudge.org/external/2/272.pdf)
